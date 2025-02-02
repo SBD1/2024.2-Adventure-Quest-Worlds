47dece100237912b4145fc3f4e70efd16e9af766
Henrique Quenino <hcquenino@gmail.com>
2025-02-02 13:11:48 -0300

    Update DDL

--- game/config/ddl.sql
+++ game/config/ddl.sql
@@ -1,7 +1,8 @@
--- SQLBook: Code
 CREATE TABLE IF NOT EXISTS Item (
     idItem INT PRIMARY KEY,
-    tipoItem VARCHAR(20) NOT NULL
+    nomeItem VARCHAR(20) NOT NULL,
+    tipoItem BOOLEAN NOT NULL,
+    precoItem INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Usuario (
@@ -12,62 +13,67 @@ CREATE TABLE IF NOT EXISTS Usuario (
 );
 
 CREATE TABLE IF NOT EXISTS Monstro (
-    idMonstro SERIAL PRIMARY KEY,
-    tipoMonstro VARCHAR(20) NOT NULL
+    idMonstro INT PRIMARY KEY,
+    nomeMonstro VARCHAR(20) NOT NULL,
+    vidaMonstro INT NOT NULL,
+    danoMonstro INT NOT NULL,
+    defMonstro INT NOT NULL,
+    tipoMonstro BOOLEAN NOT NULL,
+    qntXP INT NOT NULL,
+
 );
 
 CREATE TABLE IF NOT EXISTS Regiao (
     idRegiao INT PRIMARY KEY,
-    nomeRegiao VARCHAR(50) NOT NULL,
-    descricaoRegiao TEXT NOT NULL
+    nomeRegiao VARCHAR(20) NOT NULL,
+    descricaoRegiao VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Sala (
     idSala INT PRIMARY KEY,
-    idRegiao INT REFERENCES Regiao(idRegiao) ON DELETE CASCADE,
-    nomeSala VARCHAR(50) NOT NULL,
+    idRegiao INT REFERENCES Regiao(idRegiao) ON DELETE SET NULL,
+    nomeSala VARCHAR(20) NOT NULL,
     salaNorte INT,
     salaSul INT,
     salaLeste INT,
     salaOeste INT
 );
+
 CREATE TABLE IF NOT EXISTS instanciaMonstro (
     idInstanciaMonstro INT PRIMARY KEY,
     idMonstro INT REFERENCES Monstro(idMonstro) ON DELETE CASCADE,
+    idRegiao INT REFERENCES Regiao(idRegiao) ON DELETE SET NULL,
     vidaAtual INT NOT NULL,
     idSala INT REFERENCES Sala(idSala) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS Minion (
     idMonstro INT PRIMARY KEY REFERENCES Monstro(idMonstro) ON DELETE CASCADE,
-    idRegiaoOrigem INT REFERENCES Regiao(idRegiao) ON DELETE CASCADE,
-    nomeMonstro VARCHAR(50) NOT NULL,
-    vidaMonstro INT NOT NULL,
-    danoMonstro INT NOT NULL,
-    defMonstro INT NOT NULL,
     quantidadeOuro INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Boss (
     idMonstro INT PRIMARY KEY REFERENCES Monstro(idMonstro) ON DELETE CASCADE,
-    nomeMonstro VARCHAR(50) NOT NULL,
-    vidaMonstro INT NOT NULL,
-    danoMonstro INT NOT NULL,
-    defMonstro INT NOT NULL,
+    idItem INT REFERENCES Item(idItem) ON DELETE SET NULL,
     mulBoss DECIMAL(5, 2) NOT NULL,
-    idItem INT REFERENCES Item(idItem) ON DELETE SET NULL
+    fraseBoss VARCHAR(200) NOT NULL
+
 );
 
 CREATE TABLE IF NOT EXISTS Classe (
     idClasse INT PRIMARY KEY,
-    nomeClasse VARCHAR(50) NOT NULL,
+    nomeClasse VARCHAR(30) NOT NULL,
     mulFisico DECIMAL(5, 2) NOT NULL,
     mulMagico DECIMAL(5, 2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Personagem (
     idPersonagem SERIAL PRIMARY KEY,
-    nomePersonagem VARCHAR(50) NOT NULL,
+    idClasse INT REFERENCES Classe(idClasse) ON DELETE SET NULL,
+    idSala INT REFERENCES Sala(idSala) ON DELETE SET NULL
+    nomePersonagem VARCHAR(20) NOT NULL,
+    nivel INT NOT NULL,
+    xpAtual INT NOT NULL,
     staminaAtualPersonagem INT NOT NULL,
     vidaAtualPersonagem INT NOT NULL,
     staminaBasePersonagem INT NOT NULL,
@@ -75,8 +81,6 @@ CREATE TABLE IF NOT EXISTS Personagem (
     defensePersonagem INT NOT NULL,
     ataqueFisico INT NOT NULL,
     ataqueMagico INT NOT NULL,
-    idClasse INT REFERENCES Classe(idClasse) ON DELETE SET NULL,
-    idSala INT REFERENCES Sala(idSala) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS Save (
@@ -86,8 +90,8 @@ CREATE TABLE IF NOT EXISTS Save (
 );
 
 CREATE TABLE IF NOT EXISTS Habilidade (
-    idHabilidade SERIAL PRIMARY KEY,
-    nomeHabilidade VARCHAR(50) NOT NULL,
+    idHabilidade INT PRIMARY KEY,
+    nomeHabilidade VARCHAR(30) NOT NULL,
     danoFisico INT NOT NULL,
     danoMagico INT NOT NULL,
     custoStamina INT NOT NULL,
@@ -96,42 +100,67 @@ CREATE TABLE IF NOT EXISTS Habilidade (
 );
 
 CREATE TABLE IF NOT EXISTS Inventario (
-    idInventario SERIAL PRIMARY KEY,
+    idPersonagem INT PRIMARY KEY REFERENCES Personagem(idPersonagem) ON DELETE CASCADE,
     espacoDisponivel INT NOT NULL,
     capacidade INT NOT NULL,
-    quantidadeOuro INT NOT NULL,
-    idPersonagem INT REFERENCES Personagem(idPersonagem) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS itemPersonagem (
-    idItemPersonagem SERIAL PRIMARY KEY,
-    idInventario INT REFERENCES Inventario(idInventario) ON DELETE CASCADE,
-    quantidadeItem INT NOT NULL,
-    idItem INT REFERENCES Item(idItem) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS itemSala (
-    idItemSala SERIAL PRIMARY KEY,
-    idSala INT REFERENCES Sala(idSala) ON DELETE CASCADE,
-    quantidadeItem INT NOT NULL,
-    idItem INT REFERENCES Item(idItem) ON DELETE CASCADE
+    quantidadeOuro INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Consumivel (
     idItem INT PRIMARY KEY REFERENCES Item(idItem) ON DELETE CASCADE,
-    nomeItem VARCHAR(50) NOT NULL,
-    valorItem DECIMAL(5, 2) NOT NULL,
     incrementoVidaAtual INT NOT NULL,
     incrementoStaminaAtual INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Equipavel (
     idItem INT PRIMARY KEY REFERENCES Item(idItem) ON DELETE CASCADE,
-    nomeItem VARCHAR(50) NOT NULL,
-    valorItem DECIMAL(5, 2) NOT NULL,
     incrementoVidaBase INT NOT NULL,
     incrementoDefesa INT NOT NULL,
     mulFisico DECIMAL(5, 2) NOT NULL,
     mulMagico DECIMAL(5, 2) NOT NULL,
-    equipado CHAR(1) CHECK (equipado IN ('s', 'n'))
+);
+
+CREATE TABLE IF NOT EXISTS InstanciaItem (
+    idInstanciaItem INT PRIMARY KEY,
+    idItem INT REFERENCES Item(idItem) ON DELETE CASCADE,
+    quantidadeItem INT NOT NULL,
+    equipado BOOLEAN NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Missao (
+    idMissao INT PRIMARY KEY,
+    nomeMissao VARCHAR(50) NOT NULL,
+    descricaoMissao VARCHAR(200) NOT NULL,
+    xpRecompensa INT NOT NULL,
+    idItem INT REFERENCES Item(idItem) ON DELETE SET NULL,
+    idMissaoSeguinte INT REFERENCES Missao(idMissao) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS ObjetivoMissao (
+    idObjetivo INT PRIMARY KEY,
+    idMissao INT REFERENCES Missao(idMissao) ON DELETE CASCADE,
+    descricaoObjetivo VARCHAR(200) NOT NULL,
+    quantidadeMeta INT NOT NULL,
+    idMonstro INT REFERENCES Monstro(idMonstro) ON DELETE SET NULL,
+    idSala INT REFERENCES Sala(idSala) ON DELETE SET NULL,
+    idRegiao INT REFERENCES Regiao(idRegiao) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS ProgressoMissao (
+    idPersonagem INT REFERENCES Personagem(idPersonagem) ON DELETE CASCADE,
+    idObjetivoMissao INT REFERENCES ObjetivoMissao(idObjetivo) ON DELETE CASCADE,
+    concluida BOOLEAN NOT NULL,
+    progressoObjetivo INT NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS Loja (
+    idLoja INT PRIMARY KEY,
+    idSala INT REFERENCES Sala(idSala) ON DELETE SET NULL,
+    nomeLoja VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Catalogo (
+    idLoja INT REFERENCES Loja(idLoja) ON DELETE CASCADE,
+    idItem INT REFERENCES Item(idItem) ON DELETE CASCADE,
+    PRIMARY KEY (idLoja, idItem)
 );
